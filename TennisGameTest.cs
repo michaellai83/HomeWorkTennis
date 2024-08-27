e865202394e5bed88f7b758e9e7ce398f91133bb
michaellai83 <michael.lai@PC-MICHAELLAI>
2024-08-27 14:21:57 +0800

    modify Create Function GetScoreResult

--- TennisGameTest.cs
+++ TennisGameTest.cs
@@ -15,16 +15,20 @@ namespace HomeWorkTennis
         [Test]
         public void T01_LoveAll()
         {
-            var actual = _tennisGame.GetScoreResult();
-            actual.Should().Be("Love All");
+            GetScoreResult("Love All");
         }
 
         [Test]
         public void T02_FifteenLove()
         {
             _tennisGame.FirstPlayerScoreTimes();
+            GetScoreResult("Fifteen Love");
+        }
+
+        private void GetScoreResult(string expect)
+        {
             var actual = _tennisGame.GetScoreResult();
-            actual.Should().Be("Fifteen Love");
+            actual.Should().Be(expect);
         }
     }
 }
